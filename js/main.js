7f5a4398a9bf259cda0b499b90396325c891aa3a
Jack Zhang <zhangmengyu10@gmail.com>
2019-06-13 18:18:49 -0700

    Updated gcp runtime configuration, reduced cost (hopefully)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 /* eslint no-unused-expressions: 0 */
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
 'use strict';
 
 let pc;
